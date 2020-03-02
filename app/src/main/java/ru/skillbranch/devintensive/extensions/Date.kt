873b7de282149e63c2a821727ae778f4b4cdae2f
Tema <9119123210@mail.ru>
2020-03-03 03:24:00 +0400

    fix: humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.lang.Math.abs
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,7 +24,7 @@ fun Date.humanizeDiff(date : Date = Date()):String {
     val timeNow = this.time
 //var timeNow = Date().add(3,TimeUnits.HOUR).time
 
-    val time = (timeNow-date.time) /1000 +1
+    val time = (timeNow-date.time) / 1000
     val state = when (time) {
         in 0..1 -> "только что"
         in 1..45 -> "через несколько секунд"
@@ -32,14 +33,14 @@ fun Date.humanizeDiff(date : Date = Date()):String {
         in 45*60..75*60 -> "через час"
         in 75*60..22*60*60  -> "через ${time/60/60} часов"
         in 22*60*60..26* 60*60  ->  "через день"
-        in 22*60*60..360* 60*60 -> "более чем ${time / 60 / 60 / 24} дней назад"
+        in 22*60*60..360* 60*60 -> "более чем ${kotlin.math.abs(time / 60 / 60 / 24)} дней назад"
         in 360*60*60..Long.MAX_VALUE -> "более чем ${time/ 60/60/24} дней назад"
         in -1..0 -> "только что"
         in -45..-1 -> "несколько секунд назад"
         in -75..-45 -> "минуту назад"
         in -45*60..-75 -> "${kotlin.math.abs(time / 60)} минут назад"
         in -75*60..-45* 60 -> "час назад"
-        in -22* 60*60..-75* 60  -> "${(time/60/60)} часа назад"
+        in -22* 60*60..-75* 60  -> "${kotlin.math.abs(time / 60 / 60)} часа назад"
         in  -26* 60*60..-22*60*24  ->  "день назад"
         in -360* 60*60..-22* 60*60 -> "${kotlin.math.abs(time/ 60/60/24)} дней назад"
         else -> "более года назад"
